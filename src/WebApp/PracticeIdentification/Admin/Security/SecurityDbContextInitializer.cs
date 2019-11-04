f280626df676e2785ee9fdb2edb4c29f4a812afb
bnhanson1287 <54912724+bnhanson1287@users.noreply.github.com>
2019-11-04 11:48:15 -0700

    Re#3 Id Practice Complete

--- src/WebApp/PracticeIdentification/Admin/Security/SecurityDbContextInitializer.cs
+++ src/WebApp/PracticeIdentification/Admin/Security/SecurityDbContextInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using PracticeIdentification.Models;
+using StaffSystem.BLL;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -20,8 +21,46 @@ namespace PracticeIdentification.Admin.Security
             roleManager.Create(new IdentityRole { Name = "Registered Users" });
             #endregion
 
+            #region
+            var adminUser = new ApplicationUser
+            {
+                UserName = "WebAdmin",
+                Email = "DMIT2018@email.com",
+                EmailConfirmed = true,
+            };
 
-            base.Seed(context);
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var result = userManager.Create(adminUser, "Pa$$word1");
+            if(result.Succeeded)
+            {
+                var adminId = userManager.FindByName("WebAdmin").Id;
+
+                userManager.AddToRole(adminId, "Administrators");
+            }
+
+            var staffManager = new StaffController();
+            var staff = staffManager.ListStaff();
+            foreach (var person in staff)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = $"{person.FirstName}.{person.LastName}",
+                    Email = $"{person.FirstName}.{person.LastName}@email.com",
+                    EmailConfirmed = true
+                   
+                };
+                result = userManager.Create(user, "Pa$$word1");
+                if (result.Succeeded)
+                {
+                    var userId = userManager.FindByName(user.UserName).Id;
+                    userManager.AddToRole(userId, "Registered Users");
+                }
+            }
+                #endregion
+
+
+                base.Seed(context);
         }
     }
 }
