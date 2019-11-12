93b262b8ad6bd03fc12434814735668ecdaea6f5
Brendan N Hanson <bhanson11@nait.ca>
2019-11-12 15:29:14 -0700

    Westwind demo DTOs and Pocos added for Order Processing

---
+++ src/West Wind Demo/WestWindSystem/DataModels/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DataModels
+{
+    public class OrderItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public short Qty { get; set; }
+        public string QtyPerUnit { get; set; }
+        public short Outstanding { get; set; }
+        //TODO: Outstanding <= OrderDetails.Quantity - Sum(ManifestItems.ShipQuantity) for that product/order
+    }
+}
